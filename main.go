93d70ad0767c0461f8157d270d27bcda7bd4a0f0
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-10-02 00:30:44 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	// alex := person{"alex", "anderson"}
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// var alex person
+
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+	// fmt.Printf("%+v", jim)
+	// jimPointer := &jim
+	jim.updateName("James")
+	jim.print()
+
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
